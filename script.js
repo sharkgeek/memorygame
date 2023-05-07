4c22a84ef2cd9d0ba5f835413b6294d220fb8bba
sharkgeek <132818130+sharkgeek@users.noreply.github.com>
2023-05-07 23:54:15 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,67 @@
-const grid = document.querySelector('.grid');
-const startButton = document.querySelector('button');
+// Array to store the box colors
+var colors = [];
 
-let boxes = [];
-let score = 0;
-let level = 1;
-let numBoxes = 4;
+// Array to store the box elements
+var boxes = [];
 
-startButton.addEventListener('click', startGame);
+// Number of boxes to display
+var numBoxes = 9;
 
+// Flag to indicate whether game is in progress
+var gameInProgress = false;
+
+// Function to start the game
 function startGame() {
-    // Clear previous boxes
-    grid.innerHTML = '';
+  // Reset the game
+  colors = [];
+  boxes = [];
+  gameInProgress = true;
 
-    // Generate new boxes
-    boxes = generateBoxes(numBoxes);
-    boxes.forEach(box => grid.appendChild(box));
+  // Generate random colors for the boxes
+  for (var i = 0; i < numBoxes; i++) {
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    var color = "rgb(" + r + ", " + g + ", " + b + ")";
+    colors.push(color);
+  }
 
-    // Start timer to hide boxes
-    setTimeout(hideBoxes, 1000);
+  // Display the boxes for a short time
+  for (var i = 0; i < numBoxes; i++) {
+    var box = document.createElement("div");
+    box.style.backgroundColor = colors[i];
+    box.className = "box";
+    box.onclick = checkGuess;
+    boxes.push(box);
+    document.querySelector(".grid").appendChild(box);
+  }
+
+  setTimeout(hideBoxes, 2000);
 }
 
-function generateBoxes(num) {
-    let boxes = [];
-    for (let i = 0; i < num * num; i++) {
-        const box = document.createElement('div');
-        box.classList.add('box');
-        box.addEventListener('click', handleBoxClick);
-        boxes.push(box);
-    }
-    return boxes;
-}
-
+// Function to hide the boxes after a short time
 function hideBoxes() {
-    boxes.forEach(box => box.classList.remove('clicked'));
+  for (var i = 0; i < numBoxes; i++) {
+    boxes[i].style.backgroundColor = "#333";
+  }
 }
 
-function handleBoxClick() {
-    const boxIndex = boxes.indexOf(this);
-    if (boxIndex === level - 1) {
-        // Correct box clicked
-        this.classList.add('clicked');
-        score++;
-        level++;
-        numBoxes++;
-        setTimeout(startGame, 1000);
-    } else {
-        // Wrong box clicked
-        alert('Wrong box clicked. Game over!');
-        score = 0;
-        level = 1;
-        numBoxes = 4;
-        startGame();
+// Function to check the user's guess
+function checkGuess() {
+  if (!gameInProgress) {
+    return;
+  }
+
+  var index = boxes.indexOf(this);
+  if (colors[index] == this.style.backgroundColor) {
+    this.style.backgroundColor = "#fff";
+    this.onclick = null;
+    var remaining = boxes.filter(function(box) {
+      return box.style.backgroundColor != "#fff";
+    });
+    if (remaining.length == 0) {
+      gameInProgress = false;
+      alert("You win!");
     }
+  }
 }
